c241b1c4c761918927de95fbe4d9f0c1d2e3ad62
kevin <kevinochieng548@gmail.com>
2018-02-28 17:07:33 +0300

    [#155588631] test the modulo method

--- test_calculator.py
+++ test_calculator.py
@@ -9,6 +9,12 @@ class TDDInPython(unittest.TestCase):
         result = calc.add(5,17)
         self.assertEqual(22, result)
 
+    def test_calculator_modulo_returns_remainder(self):
+        calc = Calculator()
+        result = calc.modulo(20,3)
+        self.assertEqual(2, result)
+
+
 if __name__ == '__main__':
     unittest.main()
 
