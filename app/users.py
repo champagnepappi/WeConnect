57f9837199a9c829a742856b7b4394e77acfcf2f
kevin <kevinochieng548@gmail.com>
2018-03-06 16:03:53 +0300

    [Feature #155701096] initialise the users list

--- app/users.py
+++ app/users.py
@@ -4,6 +4,14 @@ app = Flask(__name__)
 
 class User:
 
+  users = [
+       {'id': 1, 'name': "Kevin", 'email': "K@gmail.com", 'password': 'pas123',
+        'password_confirmation': 'pas123'  },
+           {'id': 2, 'name': 'shenin','email': 'jas@gmail.com','password': '1234ab',
+            'password_confirmation': '1234ab'}]
+
   @app.route('/', methods=['GET'])
   def home():
       return jsonify({'Info': 'Welcome to WeConnect'})
+
+
