183e77b1e25bae53a45bc87684dc4ce595706b91
kevin <kevinochieng548@gmail.com>
2018-03-07 16:03:32 +0300

    [Chore #155701096] set configuration environment in run.py

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 # import os
 # from flask import Flask
-from app.users_ import app
-# app = Flask(__name__)
+# from app.users_ import app
+from app import create_app
+
+app = create_app(config_name='development')
 
 if __name__ == '__main__':
     app.run()
