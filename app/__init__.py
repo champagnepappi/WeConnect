0ff3a6d8f20fa967030566d3230fc9ba6142f72c
kevin <kevinochieng548@gmail.com>
2018-03-07 12:23:54 +0300

    [Feature #155701096] import users endpoint init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask_api import FlaskAPI
 
 from instance.config import app_config
+from app import users_
 
 def create_app(config_name):
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
-
     return app
