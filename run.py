28557f3624a2955830a86341c553f09d0d670cf4
kevin <kevinochieng548@gmail.com>
2018-03-07 12:19:19 +0300

    [Feature #155701096] import users endpoint

--- run.py
+++ run.py
@@ -1,9 +1,7 @@
-import os
-
-from app import create_app
-
-config_name = os.getenv('APP_SETTINGS')
-app = create_app(config_name)
+# import os
+# from flask import Flask
+from app.users_ import app
+# app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run()
