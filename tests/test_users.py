ecb6512bd8e0612c4d74bc4ef81e8bfe6eb75e5f
kevin <kevinochieng548@gmail.com>
2018-03-07 16:05:25 +0300

    [Chore #155701096] refactor response variable in tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,5 +1,4 @@
 import unittest
-import os
 import json
 from app import create_app
 
@@ -9,27 +8,28 @@ class UserModelTestCase(unittest.TestCase):
     def setUp(self):
         """Initialize test varibles"""
         self.app = create_app(config_name="testing")
-        self.client = self.app.test_client
+        self.client = self.app.test_client()
 
     def test_hello_there(self):
-        response = self.client().get('/')
+        response = self.client.get('/api')
         self.assertEqual(response.status_code, 200)
 
     def test_register_user(self):
-        response = self.client().post('/register',data=json.dumps(
-            dict(username="kevin",email="me@gmail.com", password="pass")),
+        response = self.client.post('auth/register',data=json.dumps(
+            dict(username="kevin",email="me@gmail.com", password="pass",
+                 password_confirmation="pass")),
             content_type="application/json")
-        self.assertEqual(response.status, 201)
+        self.assertEqual(response.status_code, 201)
 
     def test_user_login(self):
-        response = self.client().post('/login',data=json.dumps(
+        response = self.client.post('/login',data=json.dumps(
              dict(username="kevin", password="pass")),
             content_type="application/json")
         self.assertEqual(response.status, 201)
 
     def test_user_logout(self):
-        response = self.client().post('/api/auth/login',data=json.dumps(
+        response = self.client.post('/api/auth/login',data=json.dumps(
             dict(username="kevin", password="pass")),
             content_type="application/json")
-        response = self.client().get('api/auth/logout')
+        response = self.client.get('api/auth/logout')
         self.assertEqual(response.status, 200)
