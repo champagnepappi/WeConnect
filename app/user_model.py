0a87fb02ce05af985e1efb2c8fd6eb70246b78e7
kevin <kevinochieng548@gmail.com>
2018-03-08 13:03:50 +0300

    [Chore #155701096] check that user registers with matching passwords

--- app/user_model.py
+++ app/user_model.py
@@ -1,27 +1,24 @@
 from flask import Flask, jsonify
 
 app = Flask(__name__)
+users = [
+    {'user_id': 1, 'name': "Kevin", 'email': "K@gmail.com", 'password': 'pas123'},
+    {'user_id': 2, 'name': 'shenin','email': 'jas@gmail.com','password': '1234ab'}
+]
 
 class User:
     """Demonstrates methods to manipulate user data"""
 
     def __init__(self):
-         self.users = [
-         {'user_id': 1, 'name': "Kevin", 'email': "K@gmail.com", 'password': 'pas123'},
-         {'user_id': 2, 'name': 'shenin','email': 'jas@gmail.com','password': '1234ab'}
-                ]
-         self.user_info = {}
-         self.user_id = len(self.users)+1
+        self.user_info = {}
+        self.users = users
 
     def register_user(self, username, email, password, password_confirmation):
-        if password == password_confirmation:
-            self.user_info['user_id'] = self.user_id
-            self.user_info['username'] = username
-            self.user_info['email'] = email
-            self.user_info['password'] = password
-            self.users.append(self.user_info)
+        self.user_info['user_id'] = len(self.users)+1
 
-            return self.user_info
-        else:
-            return jsonify({'info': 'Password does not match'})
+        self.user_info['username'] = username
+        self.user_info['email'] = email
+        self.user_info['password'] = password
+        users.append(self.user_info)
+        return self.user_info
 
