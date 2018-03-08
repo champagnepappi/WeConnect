909eeabd173a039faf228b86b713f2dae92703b4
kevin <kevinochieng548@gmail.com>
2018-03-08 15:42:13 +0300

    [Feature #155701096] check for blank usernames

--- app/user_model.py
+++ app/user_model.py
@@ -14,6 +14,12 @@ class User:
         self.users = users
 
     def register_user(self, username, email, password, password_confirmation):
+        """
+        This method takes values passed by user and stores them
+        in the user_info dict
+        Appends the user_info to the users list
+        Then returns the user_info
+        """
         self.user_info['user_id'] = len(self.users)+1
 
         self.user_info['username'] = username
@@ -21,4 +27,18 @@ class User:
         self.user_info['password'] = password
         users.append(self.user_info)
         return self.user_info
+
+
+    def login_user(self, email, password):
+        """
+         This method loops through each user in users lists
+         then checks if email and password passed matches the ones
+         that exists in the list
+         returns user if true and message if false
+        """
+        for user in users:
+            if email == user['email'] and password == user['password']:
+                return user
+            else:
+                return "Wrong email/password combination"
 
