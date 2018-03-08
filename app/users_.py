9767b434b1dfc58179f954df1d61ac28a09048a3
kevin <kevinochieng548@gmail.com>
2018-03-08 14:10:31 +0300

    [Feature #155701096] define user login endpoint

--- app/users_.py
+++ app/users_.py
@@ -4,20 +4,31 @@ from user_model import User
 
 
 app = Flask(__name__)
-user = User()
+# user = User()
 @app.route('/api', methods=['GET'])
 def home():
     return jsonify({'message': 'Welcome to Weconnect'})
 
-@app.route('/auth/register', methods=['POST'])
+@app.route('/api/auth/register', methods=['POST'])
 def register():
+    """
+    This methods gets the json data from user
+    and uses them to register user with the register_user method
+    """
+    user = User()
     username = request.json['username']
     email  = request.json['email']
     password = request.json['password']
     password_confirmation = request.json['password_confirmation']
 
-    if not username or not email or not password or not password_confirmation:
-        return jsonify({"message": "Input cannot be blank"})
+    if not username:
+        return jsonify({"message": "Username cannot be blank"})
+    elif not email:
+        return jsonify({"message": "Email cannot be blank"})
+    elif not password:
+        return jsonify({"message": "Password cannot be blank"})
+    elif password != password_confirmation:
+        return jsonify({"message": "Password does not match"})
     elif not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
         return jsonify({"message": "Input a valid email"})
     elif len(password) < 5:
@@ -25,13 +36,27 @@ def register():
     elif [u for u in user.users if u['email']== email]:
         return jsonify({"message": "User already exists"})
 
-
-
     my_user = user.register_user(username, email, password, password_confirmation)
     return jsonify(my_user), 201
 
+@app.route('/api/auth/login', methods=['POST'])
+def login():
+    user_obj = User()
+    email = request.json['email']
+    password = request.json['password']
+    user = [u for u in user_obj.users if email == u['email'] and password == u['password']]
+    if  not user:
+        return jsonify({"message": "Invalid email/password combination"})
+
+    user_obj.login_user(email, password)
+    return jsonify({"message": "Login successful"})
+
+
+
+
 @app.route('/users', methods=['GET'])
 def get_users():
+    user = User()
     return jsonify({'users': user.users})
 
 
