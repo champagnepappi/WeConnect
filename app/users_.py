8d12fabfa0a5bf4a88d12e7a6ca5958d827b802b
kevin <kevinochieng548@gmail.com>
2018-03-08 07:59:10 +0300

    [Feature #155701096] check that email matches the email regex

--- app/users_.py
+++ app/users_.py
@@ -1,11 +1,10 @@
 from flask import Flask, jsonify, request
+import re
 from user_model import User
 
 
 app = Flask(__name__)
 user = User()
-# users = []
-
 @app.route('/api', methods=['GET'])
 def home():
     return jsonify({'message': 'Welcome to Weconnect'})
@@ -17,8 +16,13 @@ def register():
     password = request.json['password']
     password_confirmation = request.json['password_confirmation']
 
+    if not username or not email or not password or not password_confirmation:
+        return jsonify({"message": "Input cannot be blank"})
+    elif not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
+        return jsonify({"message": "Input a valid email"})
+
+
     my_user = user.register_user(username, email, password, password_confirmation)
-    # users.append(my_user)
     return jsonify(my_user), 201
 
 @app.route('/users', methods=['GET'])
@@ -26,6 +30,4 @@ def get_users():
     return jsonify({'users': user.users})
 
 
-# @app.route
-# def register():
 
