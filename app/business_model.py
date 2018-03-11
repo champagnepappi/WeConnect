85f4cb416608784e84d70936590cf9f86ba15819
kevin <kevinochieng548@gmail.com>
2018-03-11 22:12:08 +0300

    [Feature #155716699] define the init method

---
+++ app/business_model.py
@@ -0,0 +1,9 @@
+businesses = []
+
+class Business:
+    """
+    This class contains methods for manipulating business
+    data
+    """
+    def __init__(self):
+        self.businesses = businesses
