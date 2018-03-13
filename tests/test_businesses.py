f38e9c0fcb0bf1478a913b14ef627ec11c25090c
kevin <kevinochieng548@gmail.com>
2018-03-13 11:52:34 +0300

    [Chore #155716699] test business creation with blank location

--- tests/test_businesses.py
+++ tests/test_businesses.py
@@ -22,6 +22,12 @@ class BusinessModelTestCase(unittest.TestCase):
         category="Consultancy")), content_type="application/json")
         self.assertEqual(response.status_code, 400)
 
+    def test_business_creation_with_blank_location(self):
+        response = self.client.post('/api/v1/businesses',data=json.dumps(
+        dict(title="Creft designs", location="", description="lorem ipsum",
+        category="Design")), content_type="application/json")
+        self.assertEqual(response.status_code, 400)
+
     def test_should_return_all_the_businesses(self):
         response = self.client.get('/api/businesses')
         self.assertEqual(response.status, 200)
