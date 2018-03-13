9bd0b306a91ba10c3f44b8c7e08396b309ac606c
kevin <kevinochieng548@gmail.com>
2018-03-13 12:21:03 +0300

    [Feature #155716699] add case when category is blank

--- app/api.py
+++ app/api.py
@@ -118,6 +118,8 @@ def register_business():
         return jsonify({"message": "Location cannot be blank"}), 400
     elif not description or len(description.strip()) == 0:
         return jsonify({"message": "Description cannot be blank"}), 400
+    elif not category or len(category.strip()) == 0:
+        return jsonify({"message": "Category cannot be blank"}), 400
 
     business.create_business(title, location, description, category)
     return jsonify({"message": "Business created successfully"}), 201
